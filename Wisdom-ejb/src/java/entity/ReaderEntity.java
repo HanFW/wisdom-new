41e97a6d93e20918cf9067320c1c0c78ececed0d
Han Fengwei <hanfengwei96@gmail.com>
2018-04-05 23:45:18 +0800

    add setter for reader id

--- Wisdom-ejb/src/java/entity/ReaderEntity.java
+++ Wisdom-ejb/src/java/entity/ReaderEntity.java
@@ -65,6 +65,10 @@ public class ReaderEntity implements Serializable {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
